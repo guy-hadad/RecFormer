4d3d571baea62b6f4ff2048cad5d2dbc06aaad39
guy-hadad <66365205+guy-hadad@users.noreply.github.com>
2024-12-17 21:05:42 +0200

    Add files via upload

--- dataloader.py
+++ dataloader.py
@@ -28,6 +28,35 @@ class RecformerTrainDataset(Dataset):
 
 
 
+# class RecformerEvalDataset(Dataset):
+#     def __init__(self, user2train, user2val, user2test, mode, collator: EvalDataCollatorWithPadding):
+#         self.user2train = user2train
+#         self.user2val = user2val
+#         self.user2test = user2test
+#         self.collator = collator
+
+#         if mode == "val":
+#             self.users = list(self.user2val.keys())
+#         else:
+#             self.users = list(self.user2test.keys())
+
+#         self.mode = mode
+
+#     def __len__(self):
+#         return len(self.users)
+
+#     def __getitem__(self, index):
+#         user = self.users[index]
+#         seq = self.user2val[user][:-1]
+#         label = self.user2val[user]
+#         return seq, label
+
+#     def collate_fn(self, data):
+
+#         return self.collator([{'items': line[0], 'label': line[1]} for line in data])
+    
+
+
 class RecformerEvalDataset(Dataset):
     def __init__(self, user2train, user2val, user2test, mode, collator: EvalDataCollatorWithPadding):
         self.user2train = user2train
@@ -47,14 +76,13 @@ class RecformerEvalDataset(Dataset):
 
     def __getitem__(self, index):
         user = self.users[index]
-        seq = self.user2val[user][:-1]
-        label = self.user2val[user]
-
-        # seq = self.user2val[user] if self.mode == "val" else self.user2train[user] + self.user2val[user]
-        # label = self.user2val[user] if self.mode == "val" else self.user2test[user]
-        
+        # Retrieve the sequence and label; both could have variable lengths
+        seq = self.user2val[user][:-1]  # example: variable length sequence
+        label = self.user2val[user]     # example: variable length label
         return seq, label
 
-    def collate_fn(self, data):
-
-        return self.collator([{'items': line[0], 'label': line[1]} for line in data])
+    def collate_fn(self, batch):
+        # batch is a list of (seq, label) tuples
+        # If collator already handles variable lengths, just pass them through:
+        batch_data = [{'items': item[0], 'label': item[1]} for item in batch]
+        return self.collator(batch_data)
